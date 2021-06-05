af0e1deec10c17cbebcfbbe0368a982fb92bd0df
yobro2033 <80685605+yobro2033@users.noreply.github.com>
2021-06-05 12:16:09 +0700

    Update Sainsburysscraping.py

--- Sainsburysscraping.py
+++ Sainsburysscraping.py
@@ -1,1 +1,13 @@
 from bs4 import BeautifulSoup
+soup = BeautifulSoup(req.text, 'html.parser')
+
+productName = input('Please enter the product you want to find: ')
+urlProduct = 'https://www.sainsburys.co.uk/gol-ui/SearchDisplayView?filters[keyword]='+productName
+req = requests.get(urlProduct)
+
+productTitle = soup.h1['pd_header']
+descriptionProduct = soup.findAll('p',attrs={'section', class_='pd__description'})
+retailPrice = = soup.findAll('p',attrs={'section', class_='pd__cost__total undefined'})
+unitPrice = = soup.findAll('p',attrs={'section', class_='pd__cost__per-unit'})
+
+print(productTitle, descriptionProduct, retailPrice, unitPrice)
